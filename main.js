e77519c07fb249db9b2fbaa59911367e4ec26b0e
Terrance Lewis <terrance4869@gmail.com>
2020-08-08 19:16:57 -0400

    Init Commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let input = document.querySelector("input")
+let button = document.querySelector("button")
+let img = document.querySelector("img")
+
+
+
+
+button.addEventListener("click",() => {
+    button.disabled = true
+    let value = Array.from(input.value)
+    let i = 0
+    let interval = setInterval(() => {
+        if(i < value.length){
+            img.src = "StrangerThingsPhotos/" + value[i] + ".jpg"
+            console.log("x")
+            i++
+            console.log(i)
+        } else {
+            i = 0;
+            button.disabled = false
+        }      
+    }, 1000)
+    setTimeout(() => {
+        clearInterval(interval);
+        img.src = "StrangerThingsPhotos/default.jpg"
+    }, (value.length * 1000) + 1000)
+})
